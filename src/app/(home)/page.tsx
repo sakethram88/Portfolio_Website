9e9bf951564608253bb4ff45bc63328ea5c5dceb
Saketh Ram <sakethram88>
2025-04-08 14:28:32 -0400

    Aligned margins

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,10 +5,10 @@ import { LampDemo } from "@/components/ui/lamp";
 export default function Home() {
   return (
     <div className="w-full m-0 p-0">
-      <div className="m-0 p-0">
+      <div className="-mb-100 relative z-10 p-0">
         <LampDemo />
       </div>
-      <div className="m-0 p-0">
+      <div className="-mt-130 relative z-20 p-0">
         <TypewriterEffectSmoothDemo />
       </div>
       <div className="m-0 p-0">
--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -15,7 +15,7 @@ export function LampDemo() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mb-0 mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        className="mb-0 -mt-20 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         Saketh Ram <br /> Josyabhatla
         
